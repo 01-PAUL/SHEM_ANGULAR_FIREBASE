b8eb1277bf429e7c6fee1e9bd20b12ca178e165c
01-PAUL <paulponcehuaranga@gmail.com>
2024-12-02 16:22:45 -0500

    Terminado

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,17 +1,20 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './principal.component.html',
-  styleUrl: './principal.component.css'
+  styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent implements OnInit {
   authService = inject(AuthService);
+
+  // Número de imágenes
+  totalImages = 3;
+  currentImageIndex = 0;
 
   ngOnInit(): void {
     this.authService.user$.subscribe((user) => {
@@ -24,8 +27,20 @@ export class PrincipalComponent implements OnInit {
         this.authService.currentUserSig.set(null);
       }
     });
+
+    // Inicia el carrusel automático
+    this.startCarousel();
   }
-  
+
+  // Función para iniciar el carrusel
+  startCarousel() {
+    setInterval(() => {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.totalImages;
+    }, 3000); // Cambiar cada 3 segundos
+  }
+
+  // Obtener la clase dinámica
+  getImageClass(): string {
+    return `image${this.currentImageIndex + 1}`;
+  }
 }
-
-
